8d67cb10e4528210a9219d7f20b7689c33308938
Johannes Kliemann <jk@jkliemann.de>
2022-12-02 12:29:56 +0100

    Solution for day 2 part 2

--- Ada/src/day_2022_2.adb
+++ Ada/src/day_2022_2.adb
@@ -12,9 +12,15 @@ is
       ('A' => ('X' => 3, 'Y' => 6, 'Z' => 0),
        'B' => ('X' => 0, 'Y' => 3, 'Z' => 6),
        'C' => ('X' => 6, 'Y' => 0, 'Z' => 3));
+   Results_2 : constant Shape_Points := ('X' => 0, 'Y' => 3, 'Z' => 6);
+   Shapes_2 : constant Result_Points :=
+      ('A' => ('X' => 3, 'Y' => 1, 'Z' => 2),
+       'B' => ('X' => 1, 'Y' => 2, 'Z' => 3),
+       'C' => ('X' => 2, 'Y' => 3, 'Z' => 1));
    Line : String (1 .. 3);
    Last : Natural;
    Score : Natural := 0;
+   Score_2 : Natural := 0;
 begin
    while not Ada.Text_IO.End_Of_File loop
       Ada.Text_IO.Get_Line (Line, Last);
@@ -27,6 +33,8 @@ begin
             Result : constant Natural :=
                Results (Line (Line'First), Line (Line'Last));
             Points : constant Natural := Shapes (Line (Line'Last));
+            Result_2 : constant Natural := Results_2 (Line (Line'Last));
+            Points_2 : constant Natural := Shapes_2 (Line (Line'First), Line (Line'Last));
          begin
             if Natural'Last - Score > Result + Points then
                Score := Score + Result + Points;
@@ -34,8 +42,15 @@ begin
                Ada.Text_IO.Put_Line ("Max score reached:" & Score'Image);
                return;
             end if;
+            if Natural'Last - Score_2 > Result_2 + Points_2 then
+               Score_2 := Score_2 + Result_2 + Points_2;
+            else
+               Ada.Text_IO.Put_Line ("Max score 2 reached:" & Score_2'Image);
+               return;
+            end if;
          end;
       end if;
    end loop;
    Ada.Text_IO.Put_Line ("Score:" & Score'Image);
+   Ada.Text_IO.Put_Line ("Score 2:" & Score_2'Image);
 end Day_2022_2;
