25afb337056e867ae05aa5aa7cbaf599d75f37c7
Johannes Kliemann <jk@jkliemann.de>
2022-12-09 14:18:33 +0100

    Solution day 4 part 2

--- Ada/src/day_2022_4.adb
+++ Ada/src/day_2022_4.adb
@@ -21,6 +21,14 @@ is
                  or else (L.First <= R.First
                           and then L.Last >= R.Last)));
 
+   function Overlaps (L, R : Assignment) return Boolean is
+      (L.Valid
+       and then R.Valid
+       and then ((L.Last >= R.First
+                  and then L.First <= R.Last)
+                 or else (R.Last >= L.First
+                          and then R.First <= L.Last)));
+
    function Get_Assignment (S : String;
                             E : Basalt.Strings.String_Slice)
    return Assignment with
@@ -51,17 +59,31 @@ is
    Last : Natural;
    Elf_1, Elf_2 : Basalt.Strings.String_Slice;
    Contained_Assignments : Natural := 0;
+   Overlapping_Assignments : Natural := 0;
 begin
    while not Ada.Text_IO.End_Of_File loop
       Ada.Text_IO.Get_Line (Line, Last);
       Basalt.Strings.Split (Line (Line'First .. Last), ',', Elf_1, Elf_2);
-      if
-         Contained (Get_Assignment (Line, Elf_1), Get_Assignment (Line, Elf_2))
-         and then Contained_Assignments < Natural'Last
-      then
-         Contained_Assignments := Contained_Assignments + 1;
-      end if;
+      declare
+         Assignment_1 : constant Assignment := Get_Assignment (Line, Elf_1);
+         Assignment_2 : constant Assignment := Get_Assignment (Line, Elf_2);
+      begin
+         if
+            Contained (Assignment_1, Assignment_2)
+            and then Contained_Assignments < Natural'Last
+         then
+            Contained_Assignments := Contained_Assignments + 1;
+         end if;
+         if
+            Overlaps (Assignment_1, Assignment_2)
+            and then Overlapping_Assignments < Natural'Last
+         then
+            Overlapping_Assignments := Overlapping_Assignments + 1;
+         end if;
+      end;
    end loop;
    Ada.Text_IO.Put_Line ("Contained Assignments:"
                          & Contained_Assignments'Image);
+   Ada.Text_IO.Put_Line ("Overlapping Assinments:"
+                         & Overlapping_Assignments'Image);
 end Day_2022_4;
